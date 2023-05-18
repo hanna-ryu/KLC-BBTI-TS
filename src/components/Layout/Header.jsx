aa81a7031337ffb192f6b411a3dd4e3c77d1b12a
Hanna <foureaf@gmail.com>
2023-05-19 00:24:48 +0900

    feat: 로고 누르면 홈으로 돌아가는 기능 추가

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,7 +2,9 @@ function Header() {
   return (
     <header style={{ border: 'solid 2px #b38631' }}>
       <div style={{ border: 'solid 2px #b38631' }}>
-        <img src="/KLC_LOGO.png"></img>
+        <a href="http://localhost:3000">
+          <img src="/KLC_LOGO.png" />
+        </a>
       </div>
     </header>
   );
