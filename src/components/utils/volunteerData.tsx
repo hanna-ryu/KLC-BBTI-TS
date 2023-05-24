2f56119176bc9480a6047ccdfbc963e2e2fee227
Ubuntu <azureuser@hanna.bqghvz4fin1uzj1g1f1xv1m0xa.syx.internal.cloudapp.net>
2023-05-24 14:18:51 +0000

    feat : 봉사처 data 설명 추가

--- src/components/utils/volunteerData.tsx
+++ src/components/utils/volunteerData.tsx
@@ -2,31 +2,31 @@ const volunteer = [
   {
     volunteer_name: '레거시키친',
     volunteer_location: '보광동',
-    volunteer_type: '도시락 / 김장',
+    volunteer_type: '레거시키친은 봉사처로 배달되는 도시락을 직접 만들고 포장하는 곳이에요. 용산구 보광동에 위치해있답니다! 매번 봉사마다 바뀌는 메뉴들로 어르신들의 든든한 한끼를 조리해보세요!',
     img: '/LegacyKitchen.jpg',
   },
   {
-    volunteer_name: '사랑평화의집',
+    volunteer_name: '가톨릭 사랑평화의집',
     volunteer_location: '서울역',
-    volunteer_type: '도시락 / 배달',
+    volunteer_type: '가톨릭사랑평화의집 봉사는 레거시키친에서 만든 도시락을 서울역 쪽방촌에서 어르신들께 나누어드리는 봉사에요. 가방에 도시락을 차곡차곡 쌓고 댁을 방문해서 나누어드립니다! ',
     img: '/Love.jpg',
   },
   {
     volunteer_name: '사회복지원각',
     volunteer_location: '종로구',
-    volunteer_type: '도시락 / 급식',
+    volunteer_type: '사회복지원각 봉사는 레거시키친에서 만든 도시락을 탑골공원에서 어르신들께 나누어드리는 봉사에요. 마치고 나면 탑골공원 청소도 함께 한답니다! 마음도 공원도 깨끗해지는 봉사처에요.',
     img: '/WonGak.jpg',
   },
   {
     volunteer_name: '우양재단',
     volunteer_location: '마포구',
-    volunteer_type: '식료품 배달',
+    volunteer_type: '우양재단은 어르신들 댁에 방문해 식료품을 배달하는 곳이에요. 합정 인근에서 차를 타고 출발해 3-4명의 구성원이 한 조로 움직이는 봉사랍니다. 직접 어르신들을 찾아뵙고 대화를 나누고, 사람들과 금방 친해지는 것이 매력적인 봉사처에요.',
     img: '/WooYang.jpg',
   },
   {
     volunteer_name: '쪽방상담소',
     volunteer_location: '영등포구',
-    volunteer_type: '도시락 / 연탄',
+    volunteer_type: '영등포 쪽방촌 봉사는 레거시키친에서 만든 도시락을 쪽방촌 주민분들과 어르신들께 나누어드리는 봉사에요. 직접 어르신들께 인사하며 마음을 나눠보세요!',
     img: '/Yeong2.jpg',
   },
 ];
